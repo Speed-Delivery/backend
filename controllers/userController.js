f5ef5f9ce629e5707fa31db4b48f71835cedc67a
Muna Mussa <munammahdi@gmail.com>
2023-11-09 19:09:07 +0200

    usercontroller updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,18 @@
 // controllers/userController.js
+const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+
+const createUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).json({ user });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    createUser
+};
