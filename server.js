49454a432af9586f4d42baff3234dfdeb150dd89
Muna Mussa <munammahdi@gmail.com>
2023-11-09 19:45:01 +0200

    updated serverjs to add more logging

--- server.js
+++ server.js
@@ -2,24 +2,49 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const winston = require('winston');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const userRoutes = require('./routes/userRoutes');
-const {PORT} = require('./config/serverConfig');
-const {dbUri} = require('./config/dbConfig');
+const { PORT } = require('./config/serverConfig');
+const { dbUri } = require('./config/dbConfig');
 
 const app = express();
+
+// Logger setup
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() }),
+  ],
+});
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan('combined', { stream: { write: message => logger.info(message) } }));
+
+// Removed rate limiter setup
 
 // Connect to MongoDB
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log(err));
+  .then(() => logger.info('Connected to MongoDB'))
+  .catch(err => logger.error(err));
 
 // Use the user routes
 app.use('/api/user', userRoutes);
 
+// Health check endpoint
+app.get('/health', (req, res) => res.status(200).send('OK'));
+
+// Centralized error handling
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
