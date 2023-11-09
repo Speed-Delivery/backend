570ebcc8f9f2cb29cb5a56730206cb7e7322a2eb
Muna Mussa <munammahdi@gmail.com>
2023-11-09 19:31:27 +0200

    updated server.js file

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/userController');
+
+router.post('/signup', UserController.createUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const {PORT} = require('./config/serverConfig');
+const {dbUri} = require('./config/dbConfig');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3001;
+// Connect to MongoDB
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
+// Use the user routes
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
-  console.log();
+  console.log(`Server is running on port ${PORT}`);
 });
