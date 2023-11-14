266dc865a3c70a3813ed8c96750f72caf9ebc9fe
matusala3 <matusalakiflom@gmail.com>
2023-11-15 00:32:25 +0200

    "locker databseSchema, controller and routes"

---
+++ routes/LockerRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { check } = require("express-validator");
+
+const lockersController = require("../controllers/lockerControllers");
+
+const router = express.Router();
+
+router.get("/", lockersController.getLockers);
+
+router.get("/:city", lockersController.getLockersByCity);
+
+router.post(
+  "/",
+  [
+    check("lockerNumber").not().isEmpty(),
+    check("lockerLocation").not().isEmpty(),
+    check("lockerStatus").not().isEmpty(),
+  ],
+  lockersController.initializeLockers
+);
+
+router.put(
+  "/:lockerId",
+  [check("lockerStatus").isIn(["available", "occupied"])],
+  lockersController.updateLockerById
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 // server.js is the entry point for the application.
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const winston = require('winston');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const userRoutes = require('./routes/userRoutes');
-const { PORT } = require('./config/serverConfig');
-const { dbUri } = require('./config/dbConfig');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const winston = require("winston");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRoutes = require("./routes/userRoutes");
+const lockersRoutes = require("./routes/LockerRoutes");
+const { PORT } = require("./config/serverConfig");
+const { dbUri } = require("./config/dbConfig");
 
 const app = express();
 
 // Logger setup
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   format: winston.format.json(),
   transports: [
     new winston.transports.Console({ format: winston.format.simple() }),
@@ -25,27 +26,34 @@ const logger = winston.createLogger({
 app.use(cors());
 app.use(bodyParser.json());
 app.use(helmet());
-app.use(morgan('combined', { stream: { write: message => logger.info(message) } }));
+app.use(
+  morgan("combined", { stream: { write: (message) => logger.info(message) } })
+);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => logger.info('Connected to MongoDB'))
-  .catch(err => logger.error(err));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((err) => logger.error(err));
 
 // Use the user routes
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/lockers", lockersRoutes);
 
 // Health check endpoint
-app.get('/health', (req, res) => res.status(200).send('OK'));
+app.get("/health", (req, res) => res.status(200).send("OK"));
 
 // Centralized error handling
 app.use((err, req, res, next) => {
   // Check for duplicate key error
   if (err && err.code === 11000) {
-    res.status(409).send({ error: 'Username already exists.' });
+    res.status(409).send({ error: "Username already exists." });
   } else {
     logger.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).send("Something broke!");
   }
 });
 
