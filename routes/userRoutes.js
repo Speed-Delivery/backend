31110e180015635a0dd0f04a0833a77b6d12d9a4
Muna Mussa <munammahdi@gmail.com>
2023-11-18 20:22:04 +0200

    routes updated to use restfull best practices

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
-// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
+const authenticate = require('../middleware/authenticate');
+const authorize = require('../middleware/authorize');
 
-router.post('/signup', UserController.createUser);
-router.post( '/signin', UserController.signInUser);
-router.get('/profile', UserController.getUserProfile);
-router.put('/profile', UserController.updateUserProfile);
+// Public routes
+router.post('/', UserController.createUser); // Register a new user
+router.post('/signin', UserController.signInUser); // Sign in
+
+// Private routes
+router.get('/allusers', authenticate, authorize('admin'), UserController.getAllUsers); // Get all users (admin only)
+router.get('/', authenticate, authorize('admin'), UserController.getAllUsers); // Alternative route for getting all users
+router.get('/:userId', authenticate, UserController.getUser); // Get specific user
+router.put('/:userId', authenticate, authorize('admin', 'user'), UserController.updateUser); // Update user
+router.delete('/:userId', authenticate, authorize('admin'), UserController.deleteUser); // Delete user
 
 module.exports = router;
