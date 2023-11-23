95b8842f3c46a968883811bd54f17e3e15242b3d
Muna Mahadi <munammahadi@gmail.com>
2023-11-23 21:26:37 +0200

    Refactored to consumer app

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,10 +1,19 @@
-const authorize = (...allowedRoles) => {
+const authorize = () => {
     return (req, res, next) => {
-        if (!req.user || !allowedRoles.includes(req.user.role)) {
-            return res.status(403).json({ message: 'Forbidden, insufficient permissions' });
-        }
-        next();
+      if (!req.user) {
+        return res.status(403).json({ message: 'Forbidden, insufficient permissions' });
+      }
+  
+      const requestedUserId = req.params.userId;
+      const tokenUserId = req.user.userId; // Assuming user ID is stored in the 'userId' field of the token payload
+  
+      if (requestedUserId !== tokenUserId.toString()) { // Convert to string for strict comparison
+        return res.status(403).json({ message: 'Forbidden, insufficient permissions' });
+      }
+      
+      next();
     };
-};
-
-module.exports = authorize;
+  };
+  
+  module.exports = authorize;
+  
