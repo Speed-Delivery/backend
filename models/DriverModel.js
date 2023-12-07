c5838980ba37d20d846ef64cd66d40ee79c8993a
matusala3 <matusalakiflom@gmail.com>
2023-12-08 01:05:44 +0200

    v2-01

--- models/DriverModel.js
+++ models/DriverModel.js
@@ -1,49 +1,49 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Create a schema
 const driverSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true, // Ensure uniqueness
-        trim: true,
-        lowercase: true,
-        match: [/^[a-zA-Z0-9]+$/, 'is invalid'] // Only alphanumeric characters
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        trim: true,
-        lowercase: true,
-        match: [
-          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-          'Invalid email format'
-        ]
-      },      
-    password: {
-        type: String,
-        required: [true, 'Password is required']
-    },               
-   registeredAt: { 
-    type: Date, 
-    default: Date.now },          
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true, // Ensure uniqueness
+    trim: true,
+    lowercase: true,
+    match: [/^[a-zA-Z0-9]+$/, "is invalid"], // Only alphanumeric characters
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Invalid email format",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  registeredAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-driverSchema.pre('save', async function(next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 10);
-    }
-    next();
+driverSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
 });
 
-driverSchema.methods.comparePassword = async function(candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+driverSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-
-const Driver = mongoose.model('Driver', driverSchema, 'newdrivers');
+const Driver = mongoose.model("Driver", driverSchema);
 
 module.exports = Driver;
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,49 +1,49 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Create a schema
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true, // Ensure uniqueness
-        trim: true,
-        lowercase: true,
-        match: [/^[a-zA-Z0-9]+$/, 'is invalid'] // Only alphanumeric characters
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        trim: true,
-        lowercase: true,
-        match: [
-          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-          'Invalid email format'
-        ]
-      },      
-    password: {
-        type: String,
-        required: [true, 'Password is required']
-    },               
-   registeredAt: { 
-    type: Date, 
-    default: Date.now },          
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true, // Ensure uniqueness
+    trim: true,
+    lowercase: true,
+    match: [/^[a-zA-Z0-9]+$/, "is invalid"], // Only alphanumeric characters
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Invalid email format",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  registeredAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-userSchema.pre('save', async function(next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 10);
-    }
-    next();
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
 });
 
-userSchema.methods.comparePassword = async function(candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-
-const User = mongoose.model('User', userSchema, 'newusers');
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
